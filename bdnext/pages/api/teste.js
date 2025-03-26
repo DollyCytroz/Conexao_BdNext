175218fae3c082fac9e564a59c2795aff92c2fc2
DollyCytroz <kauemcarreiro11@gmail.com>
2025-03-26 20:17:27 -0300

    Banco em Next

---
+++ bdnext/pages/api/teste.js
@@ -0,0 +1,19 @@
+import mysql from 'mysql2';
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'teste'
+});
+
+export default function handler (req, res) {
+    connection.query('SELECT * FROM usuario', (error, results) => {
+        if (error) {
+            console.error('Erro na consulta:', error);
+            return res.status(500).json({error: 'Erro ao buscar dados.'});
+        }
+
+        res.status(200).json(results); 
+    }); 
+}
